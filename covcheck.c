25061eef072c4fa5b020c1eb2c2e472503c4cfe9
faragon <faragon.github@gmail.com>
2018-09-06 20:21:30 +0200

    frr_1473088: Overflowed array index write

--- covcheck.c
+++ covcheck.c
@@ -7,7 +7,20 @@
  * Released under the BSD 3-Clause License (see the LICENSE)
  */
 
+#include <inttypes.h>
 #include <stdio.h>
+#include <string.h>
+
+void frr_1473088(uint32_t c, char *t)
+{
+	char b1[256], b2[256];
+
+	snprintf(b1, sizeof(b1), "\nError %" PRIu32 " - %s", c, t);
+	memset(b2, '=', strlen(b1));
+	b2[strlen(b1) - 1] = '\0';
+
+	printf("%s %s\n", b1, b2);
+}
 
 int main()
 {
